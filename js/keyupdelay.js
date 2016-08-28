77ec79555dee58494f82313e4c67369ce58fc95c
mostfa <benellefimostfa@gmail.com>
2016-08-28 13:07:32 +0100

    fix phpcs

--- js/keyupdelay.js
+++ js/keyupdelay.js
@@ -1,18 +1,19 @@
 (function ($, Drupal, drupalSettings) {
+    'use strict'
   Drupal.behaviors.unique_field_ajax = {
     attach: function (context, settings) {
       var input_selector = drupalSettings.unique_field_ajax.id;
       var typingTimer;
       var doneTypingInterval = 1500;
-        $(input_selector).on('keyup', function (e) {
-          clearTimeout(typingTimer);
-          if ($(this).val) {
-            var trigid = $(this);
-            typingTimer = setTimeout(function(){
-              trigid.triggerHandler('finishedinput');
-            }, doneTypingInterval);
-          }
-        });
+      $(input_selector).on('keyup', function (e) {
+        clearTimeout(typingTimer);
+        if ($(this).val) {
+          var trigid = $(this);
+          typingTimer = setTimeout(function () {
+            trigid.triggerHandler('finishedinput');
+          }, doneTypingInterval);
+        }
+      });
     }
   };
 })(jQuery, Drupal, drupalSettings);
